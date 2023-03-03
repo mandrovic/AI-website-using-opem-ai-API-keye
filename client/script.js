e57ab92263588133f25c00865544a0bef1042d7d
mandrovic <125316865+mandrovic@users.noreply.github.com>
2023-03-03 23:20:46 +0300

    updt

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://stupide-ai00.onrender.com'
+    const response = await fetch('http://localhost:5000'
     , {
         method: 'POST',
         headers: {
